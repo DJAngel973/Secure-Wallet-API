05e139e5f9775dcdf24359b8d18da4e664c584a7
DJAngel973 <angel.djuancho@gmail.com>
2026-04-06 22:04:41 -0500

    refactor(common): add LogSanitizer utilily class - centralize log injection prevention (OWASP A09)

--- src/main/java/com/wallet/secure/auth/controller/AuthController.java
+++ src/main/java/com/wallet/secure/auth/controller/AuthController.java
@@ -10,7 +10,6 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
--- src/main/java/com/wallet/secure/user/controller/UserController.java
+++ src/main/java/com/wallet/secure/user/controller/UserController.java
@@ -1,14 +1,12 @@
 package com.wallet.secure.user.controller;
 
 import com.wallet.secure.common.response.ApiResponse;
-import com.wallet.secure.user.dto.RegisterRequest;
 import com.wallet.secure.user.dto.UpdateProfileRequest;
 import com.wallet.secure.user.dto.UserResponse;
 import com.wallet.secure.user.service.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
